102b62c692a20e1cbe949602e4d81c50c4018db7
Alfred Ayertey <Alfred@maximnyansa.org>
2023-11-07 09:07:25 +0000

    create Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,12 +2,14 @@
     <div>
         <LandingPage />
         <LandingWhatCanIWin />
+        <LandHowItWork />
     </div>
 </template>
 
 <script setup>
 import LandingPage from '../components/LandingPage.vue';
 import LandingWhatCanIWin from '../components/LandingWhatCanIWin.vue';
+import LandHowItWork from '../components/LandHowItWork.vue';
 </script>
 
 <style scoped>
